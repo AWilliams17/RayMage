366177577b2f728940c2c9d139781079e9d0bf24
Austin Williams <awilliams17411@gmail.com>
2018-07-24 17:53:51 -0500

    planning

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
+/*
+ * http://sdl.beuc.net/sdl.wiki/SDL_Rect
+ *  "A SDL_Rect defines a rectangular area of pixels.
+ *  It is used by SDL_BlitSurface to define blitting regions and by several other video functions.
+ *
+ *  http://sdl.beuc.net/sdl.wiki/SDL_UpdateRect
+ *   "SDL_UpdateRect -- Makes sure the given area is updated on the given screen."
+ *
+ *  http://sdl.beuc.net/sdl.wiki/SDL_BlitSurface
+ *   "This performs a fast blit from the source surface to the destination surface."
+ *
+ *  So using ray casting, use these methods to figure out where to draw rectangles, and blit them using blitsurface?
+ */
+
+
 #include <iostream>
 #include "SDL.h"
-#include "player.h"
+#include "test_map.h"
 
 //ToDo: Don't hardcode these
 const int SCREEN_WIDTH = 640;
@@ -19,7 +34,6 @@ int init(SDL_Window** sdlWindow, SDL_Surface** sdlContainer, SDL_Renderer** sdlR
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
         }
         else {
-
             *sdlContainer = SDL_GetWindowSurface(*sdlWindow); //Grab the surface contained by the window
             *sdlRenderer = SDL_CreateRenderer(*sdlWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
             if(*sdlRenderer == nullptr) {
@@ -48,23 +62,22 @@ void close(SDL_Window** sdlWindow, SDL_Surface** sdlContainer){
 }
 
 int main() {
+    bool game_loop = true;
     SDL_Window* gWindow = nullptr; //Window to render to
     SDL_Surface* gScreenSurface = nullptr; //Surface contained by the window
     SDL_Renderer* gRenderer = nullptr; //The renderer
-    bool game_loop = true;
 
     if (init(&gWindow, &gScreenSurface, &gRenderer) == 0){
         printf("Failed to initialize RayMage.\n");
     }
     else{
+        //Set the renderer to initially be black, and then output its state.
         SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
         SDL_RenderClear(gRenderer);
         SDL_RenderPresent(gRenderer);
-
         //Start handling the game loop
         SDL_Event e; //Event queue
         while (game_loop){
-
             while( SDL_PollEvent( &e ) != 0 ){ //Grab most recent event in queue; process it.
                 if( e.type == SDL_QUIT ) {
                     game_loop = false;
