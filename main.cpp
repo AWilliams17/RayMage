c255bf2714aeab33cce3bc5493748084739bb4dc
Austin Williams <awilliams17411@gmail.com>
2018-07-28 06:47:41 -0500

    cleaned up some

--- main.cpp
+++ main.cpp
@@ -4,25 +4,18 @@
 
 int main() {
     bool game_loop = true;
-
+    KeyScanner keyScanner;
+    
     Renderer renderer("RayMage", 640, 480, false);
     renderer.clearScreen();
-    renderer.drawLineVertical(55, 100, 200, renderer.colors.RED);
-    renderer.redraw();
-
-    KeyScanner keyScanner;
 
     while (game_loop) {
         keyScanner.scanKeys();
 
         if (keyScanner.exitBtnPressed()){
-            printf("Test");
-            game_loop = false;
-        }
-        if (keyScanner.isKeyDown(SDLK_w)) {
-            printf("Test");
             game_loop = false;
         }
     }
+
     return 0;
 }
